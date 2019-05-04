75c856bba1bf22cf52272a67befa8b10382d7fbd
Abhiram <abhiram_cvns@hotmail.com>
2019-05-05 02:29:43 +0530

    clusters.py

--- iris_clustering.py
+++ iris_clustering.py
@@ -63,6 +63,55 @@ plt.scatter(model.cluster_centers_[0][0], model.cluster_centers_[0][2], marker='
 '''
 
 
+#---------------------------------------------
+
+
+
+#importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('iris.csv')
+
+dataset.head()
+
+x = dataset.iloc[:,[1,2,3,4]].values
+
+from sklearn.cluster import KMeans
+wcss = []
+
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+    
+
+plt.plot(range(1,11), wcss)    
+    
+#number of clusters = 3
+
+kmeans = KMeans(n_clusters=3)
+y_kmeans = kmeans.fit_predict(x)
+
+
+
+
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], s = 100, c = 'yellow', label = 'Centroids')
+
+plt.legend()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
