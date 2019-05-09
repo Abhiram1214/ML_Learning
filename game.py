6b970f42bb5cd9bf24bd3b3dc81704b94d624e63
Abhiram <abhiram_cvns@hotmail.com>
2019-05-09 23:50:43 +0530

    telephone book
    added options to add, display and modify telephone book

--- game.py
+++ game.py
@@ -17,6 +17,8 @@ class address_book:
         self.number = number
         
         self.contacts_list[self.name] = self.number
+        
+        self.display_contacts()
     
     def display_contacts(self):
         
@@ -24,7 +26,8 @@ class address_book:
             print(key)
             print(value)            
             print("------------")
-        
+            
+            self.options()
         
     def modify_contact(self, update_name):
         
@@ -41,13 +44,59 @@ class address_book:
         else:
             print("contact not in the book")            
     
-        
-        
+    def options(self):
+        print("Enter 1 for adding contacts \n")
+        print("Enter 2 for displaying contacts \n")
+        print("Enter 3 for modifying contacts \n")
+        option = int(input("please enter an option"))
+             
+                
+        if option == 1:
+           option_name = input("Enter the name: ")
+           option_num = input("enter a number: ")
+           self.add_contact(option_name, option_num)
+        elif option == 2:
+            self.display_contacts()
+        elif option== 3:
+            option_mod_name = input("Enter the name: ")
+            #option_mod_num = input("enter a number")
+            self.modify_contact(option_mod_name)
+        else:
+            print("incorrect")
+            
         
         
 contact = address_book()        
 
-contact.add_contact("kaki", 87779)
+contact.add_contact("abhi", 1255)
 contact.display_contacts()
 contact.modify_contact('abhi')
+
+
+contact.options()
+contact.add_contact("abhi", 1255)
+contact.add_contact("kaki", 111)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
