c047e4e717d057314b21d1b011ffee0bc6c4db9d
Abhiram <abhiram_cvns@hotmail.com>
2019-05-13 06:11:22 +0530

    createed a frame

--- color_game.py
+++ color_game.py
@@ -6,21 +6,23 @@ Created on Fri May 10 23:50:10 2019
 """
 
 from tkinter import *
+from tkinter import Frame
 
 root = Tk()
 
 root.title("color game")
 
 button_frame = Frame(root)
-button_frame.config(borderwidth=10, padx=150, pady=150)
-#button_frame.grid(padx=150, pady = 150)
+button_frame.pack(side=RIGHT)
+
+Button(button_frame, text="blue", fg='blue', height = 2, width=10).grid(row=0,column=0)
+Button(button_frame, text="black", fg='black',height = 2, width=10).grid(row=0,column=1)
+Button(button_frame, text="green", fg='green',height = 2, width=10).grid(row=1,column=0)
+Button(button_frame, text="red", fg='red',height = 2, width=10).grid(row=1,column=1)
+
+button_frame.config(bg='cyan')
 
 
-
-Button(root, text="blue", fg='blue', height = 2, width=10).grid(row=0,column=0)
-Button(root, text="black", fg='black',height = 2, width=10).grid(row=0,column=1)
-Button(root, text="green", fg='green',height = 2, width=10).grid(row=1,column=0)
-Button(root, text="red", fg='red',height = 2, width=10).grid(row=1,column=1)
 
 
 
