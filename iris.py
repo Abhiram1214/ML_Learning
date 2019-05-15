92695ce170a2ef3d2643ac264ab503a239267dde
Abhiram <abhiram_cvns@hotmail.com>
2019-05-15 12:21:34 +0530

    Visualization updated
    Implemented 3D and 2D data

--- iris.py
+++ iris.py
@@ -18,7 +18,7 @@ sns.set_style('whitegrid')
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.cluster import KMeans
-
+from mpl_toolkits.mplot3d import Axes3D
 
 import statsmodels.api as sm
 import sklearn
@@ -50,17 +50,34 @@ centers = model.cluster_centers_
 
 
 
-plt.scatter(X.iloc[:,0], X.iloc[:,2], c=y_kmeans)
+plt.scatter(X.iloc[:,0], X.iloc[:,1], c=y_kmeans)
 plt.scatter(X.iloc[:,1], X.iloc[:,3], c=y_kmeans)
 
 
-plt.scatter(centers[2,:], c='yellow')
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x = iris_df.iloc[:,0]
+y = iris_df.iloc[:,1]
+
+
+plt.scatter(iris_df.iloc[:,0], iris_df.iloc[:,1], c=y_kmeans)
 plt.scatter(centers[:,0], centers[:,1])
-plt.scatter(centers[:,2], centers[:,3])
+
+plt.figure('K-Means on Iris Dataset', figsize=(7,7))
+ax = plt.axes(projection = '3d')
+ax.scatter(X.iloc[:,3],X.iloc[:,0],X.iloc[:,2], c=y , cmap='Set2', s=50)
+ax.scatter(centers[0,3],centers[0,0],centers[0,2] ,c='r', s=50, label='centroid')
+ax.scatter(centers[1,3],centers[1,0],centers[1,2] ,c='r', s=50)
+ax.scatter(centers[2,3],centers[2,0],centers[2,2] ,c='r', s=50)
 
 
 
 
+
+plt.scatter(X.iloc[:,3],X.iloc[:,0], c=y , cmap='Set2', s=50)
+plt.scatter(centers[:,3], centers[:,0],c='r')
 
 
 
