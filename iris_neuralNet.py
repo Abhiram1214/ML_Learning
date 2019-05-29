13facd00a838f71d814d8ac8b4cff0e043c12a5d
Abhiram <challapalli.abhiram@gmail.com>
2019-05-30 01:35:42 +0530

    created a variable target with encoded values of target and ran confusion matrix on it

--- iris_neuralNet.py
+++ iris_neuralNet.py
@@ -15,12 +15,6 @@ dataset = pd.read_csv('Iris.csv')
 X = dataset.iloc[:,1:5].values
 y = dataset.iloc[:, 5].values
 
-
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-
-labelencoder= LabelEncoder()
-y = labelencoder.fit_transform(y)
-
 y = pd.get_dummies(dataset.Species)
 
 
@@ -38,8 +32,17 @@ classifer.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['a
 #classifer.fit(X,y, nb_epoch=110)
 classifer.fit(X,y, epochs=125)
 
+y_pred = classifer.predict_classes(X)
 
 
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+target = dataset.iloc[:, 5].values
+labelencoder= LabelEncoder()
+target = labelencoder.fit_transform(target)
+
+cm = confusion_matrix(y_pred, target)
 
 
 
