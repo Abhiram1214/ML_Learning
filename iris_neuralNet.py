fa27754294bcf72353a1d72a91faeb787019e915
Abhiram <challapalli.abhiram@gmail.com>
2019-05-30 01:24:05 +0530

    Neural net at 98% SGD

---
+++ iris_neuralNet.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 12:25:03 2019
+
+@author: abhiram
+"""
+
+import numpy as np
+import pandas as pd
+import keras
+
+dataset = pd.read_csv('Iris.csv')
+
+X = dataset.iloc[:,1:5].values
+y = dataset.iloc[:, 5].values
+
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelencoder= LabelEncoder()
+y = labelencoder.fit_transform(y)
+
+y = pd.get_dummies(dataset.Species)
+
+
+# since the output is categorical, we can use softmax function
+
+from keras.models import Sequential#sequential module to initialize ANN
+from keras.layers import Dense#Dense module to build the layers of the ANN
+
+classifer = Sequential()
+classifer.add(Dense(4, input_dim=4, kernel_initializer='uniform', activation='relu'))
+classifer.add(Dense(3, kernel_initializer='uniform', activation='sigmoid'))
+
+classifer.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+#classifer.fit(X,y, nb_epoch=110)
+classifer.fit(X,y, epochs=125)
+
+
+
+
+
+
